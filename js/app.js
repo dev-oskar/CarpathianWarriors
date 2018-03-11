aaf30af8ed6f4d98169d991d0b512170072e23ce
Oskar Pietrucha <oskar.pietrucha@gmail.com>
2018-03-11 23:26:32 +0100

    Added navigation bar and fixed .gitignore file

---
+++ js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB1kN2u1tC_GBmK0pi1sTdPYZSrWDpsIxo",
+    authDomain: "carpathianwarriorsmma.firebaseapp.com",
+    databaseURL: "https://carpathianwarriorsmma.firebaseio.com",
+    projectId: "carpathianwarriorsmma",
+    storageBucket: "carpathianwarriorsmma.appspot.com",
+    messagingSenderId: "24911101713"
+  };
+  firebase.initializeApp(config);
