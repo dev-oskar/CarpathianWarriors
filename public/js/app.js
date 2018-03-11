c2f381f3a0bd98fcbf06d1e1afb301c6d57f8201
Oskar Pietrucha <oskar.pietrucha@gmail.com>
2018-03-12 00:20:28 +0100

    Refactored website to a Firebase projec

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+(function() {
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyB1kN2u1tC_GBmK0pi1sTdPYZSrWDpsIxo",
+      authDomain: "carpathianwarriorsmma.firebaseapp.com",
+      databaseURL: "https://carpathianwarriorsmma.firebaseio.com",
+      projectId: "carpathianwarriorsmma",
+      storageBucket: "carpathianwarriorsmma.appspot.com",
+      messagingSenderId: "24911101713"
+    };
+    firebase.initializeApp(config);
+
+    // Get elements
+    const preObject = document.getElementById('object');
+
+    // Create the references
+    const dbRefObject = firebase.database().ref().child('object');
+
+    // Sync object changes
+    dbRefObject.on('value', snap => console.log(snap.val()))
+
+}());
