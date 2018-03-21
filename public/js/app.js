787565339e4b7299a994e9927a5ee82cfca6ad65
Oskar Pietrucha <oskar.pietrucha@gmail.com>
2018-03-21 10:47:31 +0100

    Refactored project, finished main page section.

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,42 @@
-(function() {
+(function($) {
+  "use strict"; // Start of use strict
 
-    // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyB1kN2u1tC_GBmK0pi1sTdPYZSrWDpsIxo",
-      authDomain: "carpathianwarriorsmma.firebaseapp.com",
-      databaseURL: "https://carpathianwarriorsmma.firebaseio.com",
-      projectId: "carpathianwarriorsmma",
-      storageBucket: "carpathianwarriorsmma.appspot.com",
-      messagingSenderId: "24911101713"
-    };
-    firebase.initializeApp(config);
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
 
-    // Get elements
-    const preObject = document.getElementById('object');
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
 
-    // Create the references
-    const dbRefObject = firebase.database().ref().child('object');
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
 
-    // Sync object changes
-    dbRefObject.on('value', snap => console.log(snap.val()))
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 
-}());
+})(jQuery); // End of use strict
